73bca21fab1da843890972816b4494de473400f4
T0liver <taschnero@edu.bme.hu>
2025-04-08 22:58:52 +0200

    Creating TextLabel Creating text label input with hint and nice lookin'

---
+++ lib/labels.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TextLabel extends StatelessWidget {
+  final TextEditingController controller;
+  final String? hint;
+
+  const TextLabel({super.key, required this.controller, this.hint});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 300),
+        child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: const TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w600,
+          ),
+          enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 1.5,
+            ),
+          ),
+        ),
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      ),
+    );
+  }
+
+}
