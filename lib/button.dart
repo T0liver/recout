8571b5250687754f6267c356589e87fc409dbb45
T0liver <taschnero@edu.bme.hu>
2025-04-08 18:22:11 +0200

    Added button class

---
+++ lib/button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final Color bgColor;
+  final Size? size;
+  final BorderSide border;
+  final VoidCallback onPressed;
+
+  const Button({
+    super.key,
+    required this.text,
+    this.bgColor = const Color(0xFFF9DC5C),
+    this.size = const Size(331, 66),
+    this.border = const BorderSide(width: 0, color: const Color(0xFFF9DC5C)),
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size?.width,
+        height: size?.height,
+        child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: bgColor,
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            side: border,
+          ),
+          elevation: 0,
+        ), // style
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                fontSize: 24,
+                color: Colors.black
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recout/button.dart';
 
 void main() {
   runApp(const RecOut());
@@ -10,10 +11,15 @@ class RecOut extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // the changes here will be just test for running and testing if the UI is working
     return MaterialApp(
       title: 'RecOut!',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+      ),
+      home: Scaffold(
+        body: Button(text: "Bejelentkezés", onPressed: () {},
+        ),
       ),
     );
   }
