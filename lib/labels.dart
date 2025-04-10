15f2881295433ee424a92fa0bfcee32366399f86
T0liver <taschnero@edu.bme.hu>
2025-04-10 09:32:00 +0200

    Created some input fields Big commit but implemented: - BoxInputLabel - NumberInputLabel - DurationChooser

--- lib/labels.dart
+++ lib/labels.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recout/l10n/l10n.dart';
 
 class TextLabel extends StatelessWidget {
   final TextEditingController controller;
@@ -114,5 +115,127 @@ class _PasswordLabelState extends State<PasswordLabel> {
       ),
     );
   }
-  
+}
+
+class BoxInputLabel extends StatelessWidget {
+  final String? placeholder;
+  final TextEditingController controller;
+
+  const BoxInputLabel({
+    super.key,
+    required this.controller,
+    this.placeholder = "...",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: placeholder,
+        hintStyle: const TextStyle(color: Colors.grey),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(24),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
+
+class NumberInputLabel extends StatelessWidget {
+  final String? placeholder;
+  final TextEditingController controller;
+
+  const NumberInputLabel({
+    super.key,
+    required this.controller,
+    this.placeholder = "...",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          isDense: true,
+          hintText: placeholder,
+          contentPadding: const EdgeInsets.symmetric(vertical: 4),
+          border: const UnderlineInputBorder(),
+        ),
+      ),
+    );
+  }
+}
+
+class DurationChooser extends StatefulWidget {
+  String duration;
+
+  DurationChooser ({
+    super.key,
+    required this.duration
+  });
+
+  @override
+  State<StatefulWidget> createState() => _DurationChooserState(duration: duration);
+}
+
+class _DurationChooserState extends State<DurationChooser> {
+  String duration;
+
+  _DurationChooserState ({
+    required this.duration
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context)!;
+    return DropdownButton<String>(
+      value: duration,
+      underline: const SizedBox(),
+      items: [l10n.hour,].map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value, style: const TextStyle(decoration: TextDecoration.underline)),
+        );
+      }).toList(),
+      onChanged: (newValue) {
+        setState(() {
+          duration = newValue!;
+        });
+      },
+    );
+  }
+}
+
+class StringInputLabel extends StatelessWidget {
+  final String? placeholder;
+  final TextEditingController controller;
+
+  const StringInputLabel({
+    super.key,
+    required this.controller,
+    this.placeholder = "...",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextField(
+        controller: controller,
+        style: const TextStyle(
+          color: Colors.grey,
+          decoration: TextDecoration.underline,
+        ),
+        decoration: const InputDecoration(border: InputBorder.none),
+      ),
+    );
+  }
 }
