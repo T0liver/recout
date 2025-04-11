4cfecff7ee2c1eae5a0d3f9844a04819cae6ca5a
T0liver <taschnero@edu.bme.hu>
2025-04-11 19:33:31 +0200

    Add dialogue to OpenActivity and refactor the classname to OpenActivityPage

--- lib/open_activity.dart
+++ lib/open_activity.dart
@@ -3,12 +3,28 @@ import 'package:recout/texts.dart';
 import 'package:recout/workout.dart';
 
 import 'button.dart';
+import 'cards.dart';
 import 'l10n/l10n.dart';
 
-class OpenActivity extends StatelessWidget {
+class OpenActivityPage extends StatefulWidget {
   final WorkOut workOut;
 
-  const OpenActivity(this.workOut, {super.key});
+  const OpenActivityPage(this.workOut, {super.key});
+
+  @override
+  State<StatefulWidget> createState() => _OpenActivityPageState();
+}
+
+class _OpenActivityPageState extends State<OpenActivityPage> {
+  late final WorkOut workOut;
+
+  bool _showDialogue = false;
+
+  void _toggle() {
+    setState(() {
+      _showDialogue = !_showDialogue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,67 +34,94 @@ class OpenActivity extends StatelessWidget {
         ? MediaQuery.of(context).size.width * 0.9
         : 700;
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 10,),
-              const BackBtn(),
-              SizedBox(height: 20),
-              Image.asset(
-                'assets/graphics/icons/icons8-strong-arm-128.png',
-                height: MediaQuery.sizeOf(context).height * 0.15,
-              ),
-              Heading(text: workOut.name, fontSize: 24, fontWeight: FontWeight.w400,),
-              const Spacer(flex: 1),
-
-              SizedBox(height: 20),
-              BodyBase(l10n.dateofworkout),
-              SizedBox(height: 10),
-              Row(
+      body: Stack(
+        children: [
+          Center(
+            child: SizedBox(
+              width: width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Spacer(),
-                  BodyStrongText('${workOut.date.year}.'),
-                  SizedBox(width: 10),
-                  BodyStrongText('${workOut.date.month}.'),
-                  SizedBox(width: 10),
-                  BodyStrongText('${workOut.date.day}.'),
-                  Spacer(),
+                  const SizedBox(height: 10,),
+                  const BackBtn(),
+                  SizedBox(height: 20),
+                  Image.asset(
+                    'assets/graphics/icons/icons8-strong-arm-128.png',
+                    height: MediaQuery.sizeOf(context).height * 0.15,
+                  ),
+                  Heading(text: workOut.name, fontSize: 24, fontWeight: FontWeight.w400,),
+                  const Spacer(flex: 1),
+
+                  SizedBox(height: 20),
+                  BodyBase(l10n.dateofworkout),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Spacer(),
+                      BodyStrongText('${workOut.date.year}.'),
+                      SizedBox(width: 10),
+                      BodyStrongText('${workOut.date.month}.'),
+                      SizedBox(width: 10),
+                      BodyStrongText('${workOut.date.day}.'),
+                      Spacer(),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  BodyBase(l10n.durationofworkout),
+                  SizedBox(height: 10),
+                  Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Spacer(),
+                        BodyStrongText('${workOut.duration}'),
+                        BodyStrongText(workOut.durationUnit),
+                        Spacer(),
+                      ]
+                  ),
+                  SizedBox(height: 20),
+                  BodyBase(l10n.locationofworkout),
+                  SizedBox(height: 10),
+                  BodyStrongText(workOut.location),
+                  Spacer(flex: 1),
+                  Row(
+                    children: [
+                      Spacer(flex: 1),
+                      IconBtn(icon: 'assets/graphics/icons/icons8-remove-32.png', onPressed: () {}),
+                      Spacer(flex: 2),
+                      IconBtn(icon: 'assets/graphics/icons/icons8-edit-32.png', onPressed: () {}),
+                      Spacer(flex: 1),
+                    ],
+                  ),
+                  Spacer(flex: 1,)
                 ],
               ),
-              SizedBox(height: 20),
-              BodyBase(l10n.durationofworkout),
-              SizedBox(height: 10),
-              Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ),
+          if (_showDialogue) ...[
+            Container(
+                color: Colors.black12.withAlpha(50),
+                child: Column(
                   children: [
                     Spacer(),
-                    BodyStrongText('${workOut.duration}'),
-                    BodyStrongText(workOut.durationUnit),
-                    Spacer(),
-                  ]
-              ),
-              SizedBox(height: 20),
-              BodyBase(l10n.locationofworkout),
-              SizedBox(height: 10),
-              BodyStrongText(workOut.location),
-              Spacer(flex: 1),
-              Row(
-                children: [
-                  Spacer(flex: 1),
-                  IconBtn(icon: 'assets/graphics/icons/icons8-remove-32.png', onPressed: () {}),
-                  Spacer(flex: 2),
-                  IconBtn(icon: 'assets/graphics/icons/icons8-edit-32.png', onPressed: () {}),
-                  Spacer(flex: 1),
-                ],
-              ),
-              Spacer(flex: 1,)
-            ],
-          ),
-        ),
-      ),
+                    Center(
+                      child: DialogueCard(title: l10n.deleteTitle,
+                        body: l10n.cantBeUndoneAccount,
+                        onYes: () {
+                          //törlés
+                          setState(() {
+                            _showDialogue = false;
+                          });
+                        },
+                        onNo: _toggle,
+                      ),
+                    ),
+                    Spacer()
+                  ],
+                )
+            )
+          ]
+        ],
+      )
     );
   }
 }
