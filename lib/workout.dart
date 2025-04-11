d230527fbbe87f63809e44d88adf8c067445c5a8
T0liver <taschnero@edu.bme.hu>
2025-04-11 11:06:10 +0200

    Add workout class Add workout data class. Or trying to

---
+++ lib/workout.dart
@@ -0,0 +1,17 @@
+class WorkOut {
+  String name;
+  DateTime date;
+  double duration;
+  String durationUnit;
+  String? location;
+  String icon;
+
+  WorkOut({
+    required this.name,
+    required this.date,
+    required this.duration,
+    required this.durationUnit,
+    this.location,
+    this.icon = 'assets/graphics/icons/icons8-strong-arm-128.png',
+  });
+}
