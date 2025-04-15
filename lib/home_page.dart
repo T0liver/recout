ecc379aa1a632d169d4903e76a71bc8a63ac4e3b
T0liver <taschnero@edu.bme.hu>
2025-04-15 18:07:48 +0200

    Add the login with firebase

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,28 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:recout/button.dart';
 import 'package:recout/cards.dart';
 import 'package:recout/globals.dart';
 import 'package:recout/l10n/l10n.dart';
 import 'package:recout/texts.dart';
+import 'package:recout/user.dart';
 import 'package:recout/workout.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
   const HomePage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late String username;
+  late String uid;
+  late String uemail;
+
+  Future<void> getLoginData() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    final userQuery = await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: user?.email)
+        .limit(1)
+        .get();
+
+    if (userQuery.docs.isEmpty || user == null) {
+      Navigator.pushNamed(context, '/first');
+    }
+
+    final userData = userQuery.docs.first.data();
+    Globals.user = UserR(username: userData['username'], email: userData['email'], userid: user!.uid);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (!Globals.loggedin()) {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         Navigator.pushReplacementNamed(context, '/first');
       });
       return SizedBox.shrink();
     }
+
+    getLoginData();
+
     final String uname;
     if (Globals.user?.username == null) {
       uname = 'kondi_geza52';
     } else {
       uname = Globals.user!.username;
+      Globals.login();
     }
     return Scaffold(
       body: Column(
