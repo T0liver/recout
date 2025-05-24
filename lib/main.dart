6d28fe358d7aae1b540177df8b3b48ea1a49f571
T0liver <taschnero@edu.bme.hu>
2025-05-24 20:38:23 +0200

    Actually solving this FRICKING problem Now the correct URL in the URL bar shows up LIKE IN A NORMAL APPLICATION Is that to much for ask from go_router? idk

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -36,7 +37,11 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
-  usePathUrlStrategy();
+
+  if (kIsWeb) {
+    usePathUrlStrategy();
+    GoRouter.optionURLReflectsImperativeAPIs = true;
+  }
 
   runApp(MultiProvider(
       providers: [
