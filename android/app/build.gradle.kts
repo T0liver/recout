ede93f2ca50bb4788999d23f767910cd81d9bd55
T0liver <taschnero@edu.bme.hu>
2025-06-16 13:14:24 +0200

    Getting secrets the right way - build.gradle.kts: getting from static paths and sysenvs not from variable bullshits - main.yml: decoding keystore with timheuer/base64-to-file and and adding secrets to env

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -10,14 +7,6 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("android/key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
-
 android {
     namespace = "hu.toliver.recout"
     compileSdk = flutter.compileSdkVersion
@@ -44,10 +33,10 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = rootProject.file("android/app/keystore.jks")
-            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+            storeFile = file("android_keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
         }
     }
 
@@ -56,7 +45,10 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isShrinkResources = true
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
